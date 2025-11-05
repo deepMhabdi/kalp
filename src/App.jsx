992a7fda2a0288c2d18702e82497e15c68d3fe46
Deep Mhabdi <dmmhabdi2701@gmail.com>
2025-11-06 01:54:33 +0530

    Add routing for contact page and update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom"
+
 import Landing from './components/Landing'
 import Navbar from './components/Navbar'
 import About from './components/About'
 import Trust from './components/Trust'
 import WhyUsSection from './components/Service'
 import Footer from './components/Footer'
+import ContactUs_Page from "./ContactUs_Page";
 
 const App = () => {
   return (
-    <div>
-      <Landing />
-      <WhyUsSection />
-      <Trust />
-      <About />
-      <Footer />
-    </div>
+    <>
+      <Navbar />   {/* keep navbar globally always visible */}
+
+      <Routes>
+        {/* Home page */}
+        <Route 
+          path="/" 
+          element={
+            <div>
+              <Landing />
+              <WhyUsSection />
+              <Trust />
+              <About />
+              <Footer />
+            </div>
+          } 
+        />
+
+        {/* Contact Page */}
+        <Route path="/contact" element={<ContactUs_Page />} />
+      </Routes>
+    </>
   )
 }
 
